17132e20513e5bafa1960415512aa84367d5ddcc
martin_covarrubias <covarrubias8080@gmail.com>
2022-12-13 15:42:39 -0600

    se realizo el footer y estilos del footer

--- repositorio/views.py
+++ repositorio/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,4 +6,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'slider.html')
+    return render(request,'index.html')
