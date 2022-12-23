b62a5009138856175967bc8ac934818e0dd834d4
martin_covarrubias <covarrubias8080@gmail.com>
2022-12-23 14:35:31 -0600

    terrcer commit cova

--- repositorio/urls.py
+++ repositorio/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
        path('registroSub/', views.registroSub),
        path('agregarDocumentos/', views.agregarDocumentos),
        
-       #te la comes joto
+       #te la comes joto tu macho el kks
+
 ]
